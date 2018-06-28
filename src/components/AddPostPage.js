99aa39f07801d417a4e6a04dc439efc88c009cd3
Taylor <15224340+ttstauss@users.noreply.github.com>
2018-06-28 15:11:26 -0600

    Add Remarkable support

--- src/components/AddPostPage.js
+++ src/components/AddPostPage.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import { startAddPost } from '../actions/posts'
 import PostForm from './PostForm'
 
 import { withStyles } from '@material-ui/core/styles'
+import { Typography } from '@material-ui/core'
 
 const styles = theme => ({
   'content-container': {
     margin: '0 auto',
     maxWidth: '80rem',
     padding: `0 ${theme.spacing.medium}`
+  },
+  link: {
+    transition: 'color .3s ease',
+    '&:hover': {
+      color: 'rgba(63, 81, 181, 1)'
+    }
+  },
+  'link__text': {
+    transition: 'color .3s ease',
+    '&:hover': {
+      color: 'rgba(63, 81, 181, 1)'
+    }
   }
 })
 
@@ -22,6 +36,19 @@ export class AddPostPage extends Component {
     const { classes } = this.props
     return (
       <div className={classes['content-container']}>
+        <a
+          className={classes.link}
+          href={'https://jonschlinkert.github.io/remarkable/demo/'}
+          target={'_blank'}
+          rel={'noopener'}
+        >
+          <Typography
+            className={classes['link__text']}
+            gutterBottom
+          >
+            Use Remarkable syntax to style your post.
+          </Typography>
+        </a>
         <PostForm onSubmit={this.onSubmit} />
       </div>
     )
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,13 @@
-import React from 'react'
-import keyIndex from 'react-key-index'
+import React, { Component } from 'react'
+import Remarkable from 'remarkable'
 import moment from 'moment'
 
 import { withStyles } from '@material-ui/core/styles'
 import { Typography, Card, CardContent } from '@material-ui/core'
 
-const styles = theme => ({
+const styles = theme => {
+  console.log(theme)
+  return ({
   'content-container': {
     margin: '0 auto',
     maxWidth: '80rem',
@@ -18,26 +20,32 @@ const styles = theme => ({
     marginBottom: `${theme.spacing.large}`
   },
   'post__body': {
-    marginBottom: `${theme.spacing.medium}`
+    ...theme.typography,
+    '& img': {
+      maxWidth: '100%'
+    }
   }
 })
-
-export const Post = ({ classes, body, title, createdAt }) => {
-  let bodyArr = body.split('\n').filter(text => text !== '')
-  bodyArr = keyIndex(bodyArr, 1)
-  return (
-    <div className={classes['content-container']}>
-      <Card className={classes.post}>
-        <CardContent>
-          <Typography variant="title" component="h1">{title}</Typography>
-          <Typography className={classes['post__caption']} variant="caption">Last updated {moment(createdAt).format('MMMM Do, YYYY')}</Typography>
-          {bodyArr.map(arr => (
-            <Typography className={classes['post__body']} key={arr.id}>{arr.value}</Typography>
-          ))}
-        </CardContent>
-      </Card>
-    </div> 
-  )
+}
+export class Post extends Component {
+  getRawMarkup() {
+    const md = new Remarkable
+    return { __html: md.render(this.props.body)}
+  }
+  render() {
+    const { classes, title, createdAt } = this.props
+    return (
+      <div className={classes['content-container']}>
+        <Card className={classes.post}>
+          <CardContent>
+            <Typography variant="title">{title}</Typography>
+            <Typography className={classes['post__caption']} variant="caption">Last updated {moment(createdAt).format('MMMM Do, YYYY')}</Typography>
+            <div className={classes['post__body']} dangerouslySetInnerHTML={this.getRawMarkup()}></div>
+          </CardContent>
+        </Card>
+      </div>
+    )
+  }
 }
 
 export default withStyles(styles)(Post)
