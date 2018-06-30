46b1d85738e6b9c845c8f47b234f0391e3d084bb
Taylor <15224340+ttstauss@users.noreply.github.com>
2018-06-30 13:31:20 -0600

    Add back link for logged in users on post page

--- src/components/PostListItem.js
+++ src/components/PostListItem.js
@@ -35,14 +35,14 @@ const styles = theme => ({
 export const PostListItem = ({ id, title, createdAt, classes, num }) => {
   return (
     <Fade in={true} {...({ timeout: 100 * num })}>
-    <Card className={classes['list-item']}>
-      <Link className={classes['list-item__link']} to={`/edit/${id}`}>
-        <CardContent className={classes['list-item__body']}>
-          <Typography variant="title">{title}</Typography>
-          <Typography variant="caption">{`Published or Updated on: ${moment(createdAt).format('MMMM Do, YYYY')}`}</Typography>
-        </CardContent>
-      </Link>
-    </Card>
+      <Card className={classes['list-item']}>
+        <Link className={classes['list-item__link']} to={`/edit/${id}`}>
+          <CardContent className={classes['list-item__body']}>
+            <Typography variant="title">{title}</Typography>
+            <Typography variant="caption">{`Published or Updated on: ${moment(createdAt).format('MMMM Do, YYYY')}`}</Typography>
+          </CardContent>
+        </Link>
+      </Card>
     </Fade>
   )
 }
--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -1,19 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import database from '../firebase/firebase'
 import Post from './Post'
 
 import CircularProgress from '@material-ui/core/CircularProgress'
 import { withStyles } from '@material-ui/core/styles'
+import { Typography } from '@material-ui/core';
 
-const styles = {
+const styles = theme => ({
   loader: {
     alignItems: 'center',
     display: 'flex',
     height: '100vh',
     justifyContent: 'center',
     width: '100vw'
+  },
+  'content-container': {
+    margin: '0 auto',
+    maxWidth: '80rem',
+    padding: `0 ${theme.spacing.medium}`
+  },
+  link: {
+    transition: 'color .3s ease',
+    '&:hover': {
+      color: 'rgba(63, 81, 181, 1)'
+    }
+  },
+  'link__text': {
+    marginTop: `${theme.spacing.medium}`,
+    transition: 'color .3s ease',
+    '&:hover': {
+      color: 'rgba(63, 81, 181, 1)'
+    }
   }
-}
+})
 
 export class PostPage extends Component {
   state = {}
@@ -35,7 +56,25 @@ export class PostPage extends Component {
     if (this.state.post) {
       const { title, body, createdAt } = this.state.post
       return (
-        <Post title={title} body={body} createdAt={createdAt} />
+        <Fragment>
+          {
+            this.props.state.auth.uid &&
+            <div className={classes['content-container']}>
+              <Link
+                className={classes.link}
+                to={`/edit/${this.props.match.params.id}`}
+              >
+                <Typography
+                  className={classes['link__text']}
+                  gutterBottom
+                >
+                  Back
+                </Typography>
+              </Link>
+            </div>
+          }
+          <Post title={title} body={body} createdAt={createdAt} />
+        </Fragment>
       )
     } else {
       return (
@@ -47,4 +86,8 @@ export class PostPage extends Component {
   }
 }
 
-export default withStyles(styles)(PostPage)
+const mapStateToProps = state => ({
+  state
+})
+
+export default connect(mapStateToProps)(withStyles(styles)(PostPage))
