84b915b407a7a72a53004e31f106a58e3bea8088
Taylor <15224340+ttstauss@users.noreply.github.com>
2018-06-29 22:32:38 -0600

    Convert LoginPage to Dialog, add window sizing support with throttle, and add styling

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,40 +2,34 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { startGoogleLogin, startRegisterUser, startEmailLogin, sendResetLink } from '../actions/auth'
 import Button from '@material-ui/core/Button'
+import _ from 'lodash'
 
-import { Card, CardContent, Typography, CardActions, TextField, Collapse, Fade } from '@material-ui/core'
+import { Typography, TextField, Collapse, Fade, Dialog, DialogContent, Backdrop } from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGoogle } from '@fortawesome/free-brands-svg-icons'
 import { faAt } from '@fortawesome/free-solid-svg-icons'
 
 const styles = theme => ({
-  'box-layout': {
-    alignItems: 'center',
-    background: 'linear-gradient(to bottom right, #3f51b5, #606fc7)',
-    display: 'flex',
-    height: '100vh',
-    justifyContent: 'center',
-    width: '100vw',
+  root: {
+    background: 'linear-gradient(to bottom right, #3f51b5, #606fc7)'
   },
-  'box-layout__box': {
-    borderRadius: '3px',
-    padding: `${theme.spacing.large} ${theme.spacing.medium} 0 ${theme.spacing.medium}`,
+  paperWidthSm: {
+    maxWidth: '420px'
+  },
+  backdrop: {
+    backgroundColor: 'transparent'
+  },
+  'login__title': {
     textAlign: 'center',
-    width: theme.spacing.unit * 50,
-    maxWidth: '25rem',
-    [theme.breakpoints.up('340')]: {
-      maxWidth: '32rem',
-    }
-  },
-  'box-layout__title': {
     padding: '0',
     margin: `0 0 ${theme.spacing.medium} 0`
   },
-  'box-layout__subheading': {
+  'login__subheading': {
+    textAlign: 'center',
     margin: `0 0 ${theme.spacing.medium} 0`
   },
-  textField: {
+  'login__textField': {
     width: '100%',
     marginBottom: theme.spacing.medium
   },
@@ -57,6 +51,7 @@ const styles = theme => ({
     marginRight: theme.spacing.small
   },
   link: {
+    textAlign: 'center',
     transition: 'color .3s ease',
     marginBottom: theme.spacing.medium,
     '&:hover': {
@@ -70,7 +65,12 @@ const styles = theme => ({
       color: 'rgba(63, 81, 181, 1)'
     }
   },
+  text: {
+    textAlign: 'center',
+    marginBottom: theme.spacing.medium
+  },
   caption: {
+    textAlign: 'center',
     marginBottom: theme.spacing.medium
   }
 })
@@ -81,7 +81,8 @@ export class LoginPage extends Component {
     password: '',
     typeOfSubmit: undefined,
     showReset: false,
-    resetSent: false
+    resetSent: false,
+    windowWidth: undefined
   }
   onEmailChange = e => {
     const email = e.target.value
@@ -139,129 +140,155 @@ export class LoginPage extends Component {
         console.log(error.message)
       })
   }
+  updateWindowWidth = _.throttle(() => {
+    this.setState({
+      windowWidth: window.innerWidth
+    })
+  }, 200)
+  componentDidMount() {
+    this.updateWindowWidth()
+    window.addEventListener('resize', this.updateWindowWidth)
+  }
+  componentWillUnmount(prevState) {
+    window.removeEventListener('resize', this.updateWindowWidth)
+  }
   render() {
     const { startGoogleLogin, classes} = this.props
     return (
-      <Fade in={true}>
-        <div className={classes['box-layout']}>
-          <Card className={classes['box-layout__box']}>
-            <CardContent style={{padding: '0'}}>
-              <Typography className={classes['box-layout__title']} variant="headline" component="h1">
-                MY MICRO BLOG
-              </Typography>
-              <Typography className={classes['box-layout__subheading']} variant="subheading">
-                Blog to your hearts content
-              </Typography>
-            </CardContent>
-            <CardActions style={{justifyContent: 'center', flexDirection: 'column'}}>
-              <form>
-                <TextField
-                  className={classes.textField}
-                  label="Email"
-                  onChange={this.onEmailChange}
-                />
-                <TextField
-                  className={classes.textField}
-                  type="password"
-                  label="Password"
-                  onChange={this.onPasswordChange}
-                />
-                <Button
-                  className={[classes.button, classes['button--email']].join(' ')}
-                  variant="contained"
-                  color="primary"
-                  size="medium"
-                  onClick={this.onLogin}
-                >
-                  <FontAwesomeIcon
-                    className={classes['icon-social']}
-                    icon={faAt}
-                  />
-                  Login with Email
+      <Dialog
+        open={true}
+        disableBackdropClick={true}
+        disableEscapeKeyDown={true}
+        fullScreen={this.state.windowWidth < 420}
+        maxWidth="sm"
+        BackdropProps={{
+          classes: {
+            root: classes.backdrop
+          }
+        }}
+        classes={{
+          paperWidthSm: classes.paperWidthSm
+        }}
+        className={classes.root}
+      >
+        <DialogContent
+          className={classes.login}
+        >
+          <Typography className={classes['login__title']} variant="headline" component="h1">
+            MY MICRO BLOG
+          </Typography>
+          <Typography className={classes['login__subheading']} variant="subheading">
+            Blog to your hearts content
+          </Typography>
+          <TextField
+            className={classes['login__textField']}
+            label="Email"
+            onChange={this.onEmailChange}
+          />
+          <TextField
+            className={classes['login__textField']}
+            type="password"
+            label="Password"
+            onChange={this.onPasswordChange}
+          />
+          <Button
+            className={[classes.button, classes['button--email']].join(' ')}
+            variant="contained"
+            color="primary"
+            size="medium"
+            onClick={this.onLogin}
+          >
+            <FontAwesomeIcon
+              className={classes['icon-social']}
+              icon={faAt}
+            />
+            Login with Email
                 </Button>
-                <Button
-                  className={[classes.button, classes['button--email']].join(' ')}
-                  variant="contained"
-                  size="medium"
-                  onClick={this.onRegister}
-                >
-                  <FontAwesomeIcon
-                    className={classes['icon-social']}
-                    icon={faAt}
-                  />
-                  Register with Email
+          <Button
+            className={[classes.button, classes['button--email']].join(' ')}
+            variant="contained"
+            size="medium"
+            onClick={this.onRegister}
+          >
+            <FontAwesomeIcon
+              className={classes['icon-social']}
+              icon={faAt}
+            />
+            Register with Email
                 </Button>
-                <Typography gutterBottom>- OR -</Typography>
-                <Button
-                  className={[classes['button--google'], classes['button']].join(' ')}
-                  variant="contained"
-                  size="medium"
-                  type="button"
-                  onClick={startGoogleLogin}
-                >
-                  <FontAwesomeIcon
-                    className={classes['icon-social']}
-                    icon={faGoogle}
-                  />
-                  Login with Google
-                </Button>
-              </form>
-              <a
-                className={classes.link}
-                type="button"
-                onClick={this.handleGetEmail}
+          <Typography
+            className={classes.text}
+            gutterBottom
+          >
+            - OR -
+          </Typography>
+          <Button
+            className={[classes['button--google'], classes['button']].join(' ')}
+            variant="contained"
+            size="medium"
+            type="button"
+            onClick={startGoogleLogin}
+          >
+            <FontAwesomeIcon
+              className={classes['icon-social']}
+              icon={faGoogle}
+            />
+            Login with Google
+          </Button>
+          <a
+            className={classes.link}
+            type="button"
+            onClick={this.handleGetEmail}
+          >
+            <Typography
+              className={classes['link__text']}
+            >
+              Forgot password?
+            </Typography>
+          </a>
+          <Collapse
+            in={this.state.showReset}
+          >
+            <form>
+              <Typography
+                variant="caption"
+                className={classes.caption}
               >
-                <Typography
-                  className={classes['link__text']}
-                >
-                  Forgot password?
-                </Typography>
-              </a>
-              <Collapse
-                in={this.state.showReset}
-              >
-                <form>
-                  <Typography
-                    variant="caption"
-                    className={classes.caption}
-                  >
-                    Please provide the email address you would like a password reset link sent to.
+                Please provide the email address you would like a password reset link sent to.
                   </Typography>
-                  <TextField
-                    className={classes.textField}
-                    label="Email"
-                    onChange={this.onEmailChange}
-                  />
-                  <Button
-                    className={[classes.button, classes['button--email']].join(' ')}
-                    variant="contained"
-                    size="light"
-                    size="medium"
-                    onClick={this.sendReset}
-                  >
-                    Send Reset
+              <TextField
+                className={classes['login__textField']}
+                label="Email"
+                onChange={this.onEmailChange}
+              />
+              <Button
+                className={[classes.button, classes['button--email']].join(' ')}
+                variant="contained"
+                size="light"
+                size="medium"
+                onClick={this.sendReset}
+              >
+                Send Reset
                   </Button>
-                </form>
-              </Collapse>
-              <Collapse
-                in={this.state.resetSent}
+            </form>
+          </Collapse>
+          <Collapse
+            in={this.state.resetSent}
+          >
+            <Fade
+              in={this.state.resetSent}
+              {...(this.state.resetSent ? { timeout: 1000 } : {})}
+            >
+              <Typography
+                variant="caption"
+                className={classes.caption}
               >
-                <Fade
-                  in={this.state.resetSent}
-                {...(this.state.resetSent ? { timeout: 1000 } : {})}
-                >
-                  <Typography
-                    variant="caption"
-                    className={classes.caption}
-                  >
-                    A reset link has been sent to your email.
-                  </Typography>
-                </Fade>
-              </Collapse>
-            </CardActions>
-          </Card>
-        </div>
-      </Fade>
+                A reset link has been sent to your email.
+              </Typography>
+            </Fade>
+          </Collapse>
+        </DialogContent>
+      </Dialog>
     )
   }
 }
